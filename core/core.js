c254d683bb29a65643e83b54f8c99007ea837847
Florian Margaine <florian@margaine.com>
2012-07-15 10:19:53 +0200

    Express config, config.json, starting pies management

--- core/core.js
+++ core/core.js
@@ -1,3 +1,60 @@
 module.exports = function() {
-    console.log( 'Tartempion running.' );
+    // Load the configuration
+    var config = loadConfig();
+    console.log( 'Configuration loaded.' );
+
+    // Load the express server
+    var app = express( config );
+
+    // Load the pies
+    loadPies();
+
+    // Run the server
+    app.listen( config.port, function() {
+        console.log( 'Tartempion listening on: '
+            + app.address().address + ':'
+            + app.address().port
+        );
+    });
 };
+
+var fs = require( 'fs' );
+
+/**
+ * Load the configuration file and return it as an object
+ */
+function loadConfig() {
+    var config = fs.readFileSync( './config.json' );
+    return JSON.parse( config );
+}
+
+function express( config ) {
+    var path = require( 'path' ),
+        express = require( 'express' ),
+        app = express.createServer();
+
+    // Configuration of express
+    app.configure( function() {
+        this.set( 'views', path.join(
+            __dirname, config.views
+        ));
+        this.set( 'view engine', config[ 'view engine' ] );
+        this.use( express.bodyParser() );
+        if ( 'session' in config ) {
+            this.use( express.session( {
+                secret: config.session.secret,
+                cookie: {
+                    maxAge: config.session.maxAge
+                }
+            }));
+        }
+    });
+
+    return app;
+}
+
+function loadPies() {
+    var pies = fs.readFileSync( './pies.json' );
+    console.log( JSON.parse( pies ) );
+}
+

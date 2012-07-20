d6cff54df04150cbe8ec50d87b7e4b17c8a7595b
Florian Margaine <florian@margaine.com>
2012-07-20 16:53:48 +0200

    Added helpers files

---
+++ helpers/helpers.js
@@ -0,0 +1,14 @@
+module.exports = {
+    /**
+     * http://expressjs.com/guide.html#app.helpers()
+     */
+    helpers: {
+    },
+
+    /**
+     * http://expressjs.com/guide.html#app.dynamichelpers()
+     */
+    dynamicHelpers: {
+    }
+};
+

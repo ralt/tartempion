7b1f2ceac7606711ddcd18c7ad9a0d30f5d64e08
Florian Margaine <florian@margaine.com>
2012-07-21 10:26:04 +0200

    Completed helpers API example.

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -3,12 +3,27 @@ module.exports = {
      * http://expressjs.com/guide.html#app.helpers()
      */
     helpers: {
+        renderScriptsTags: function( scripts ) {
+            if ( all ) {
+                return scripts.map( function( script ) {
+                    return '<script src="' + script + '"></script>';
+                }).join( '\n' );
+            }
+            else {
+                return '';
+            }
+        }
     },
 
     /**
      * http://expressjs.com/guide.html#app.dynamichelpers()
      */
     dynamicHelpers: {
+        scripts: function( req, res ) {
+            return [
+                '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'
+            ];
+        }
     }
 };
 

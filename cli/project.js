9e22dc02ce0993891336c247cd9c6f7569e77bc1
Florian Margaine <florian@margaine.com>
2012-08-18 01:01:29 +0200

    Fixed the create-project command

--- cli/project.js
+++ cli/project.js
@@ -7,8 +7,9 @@ module.exports = {
      * Create a project with the default files and folders.
      *
      * The default hierarchy can be found in the folder cli/default/
-     * of the tartempion module. The only thing to dynamically create
-     * is the package.json file.
+     * of the tartempion module.
+     *
+     * The only thing to dynamically create is the package.json file.
      */
     createProject: function( project ) {
         var current = process.cwd();
@@ -25,6 +26,26 @@ module.exports = {
             path.join( __dirname, 'default', 'project' ),
             path.join( current, project )
         );
+
+        console.log( '\nCreating the package.json file...' );
+
+        // Open the package.json file and fill it in
+        // with the correct datas.
+
+        var packagePath = path.join( current, project, 'package.json' );
+        // First, get the datas
+        var pack = JSON.parse( fs.readFileSync( packagePath ));
+
+        // Add the properties in the object
+        pack.name = project;
+        pack.version = '0.0.1';
+        pack.dependencies = {
+            'tartempion': '0.0.x'
+        };
+
+        // And write the object to the package.json file
+        // by overriding everything in it.
+        fs.writeFileSync( packagePath, JSON.stringify( pack, null, 4 ) );
 
         console.log( '\nProject "' + project + '" created.\n' );
     }

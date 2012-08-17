78309ce6b421f3a8ca869e270b8a71690d2ebce2
Florian Margaine <florian@margaine.com>
2012-08-18 01:28:55 +0200

    Added the run command

--- cli/system.js
+++ cli/system.js
@@ -1,16 +1,33 @@
-var cp = require( 'child_process' );
+var cp = require( 'child_process' ),
+    spawn = require( 'child_process' ).spawn,
+    fs = require( 'fs' );
 
 module.exports = {
     // Run tartempion
     run: function() {
+        // First check if the tartempion.js file exists
+        var files = fs.readdirSync( process.cwd() );
+        if ( !~files.indexOf( 'tartempion.js' ) ) {
+            console.error( "\nThe tartempion.js file doesn't exist." );
+            console.error( "Are you sure you're at the root of your folder?\n" );
+            process.exit( -1 );
+        }
+
+        // Spawn the process
+        var node = spawn( 'node', [ 'tartempion.js' ] );
+
+        // And send each output to stdout
+        node.stdout.on( 'data', function( data ) {
+            process.stdout.write( data );
+        });
     },
 
     // Show help
     help: function() {
-
         // Just run tartempion with the --help option
         cp.exec( 'tartempion --help',
             function( err, stdout, stderr ) {
+            if ( err ) throw err;
             process.stdout.write( stdout );
         });
     }

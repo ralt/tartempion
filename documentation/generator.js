d0a0a22bc8bb070181451e0283ad5b81738d5539
Florian Margaine <florian@margaine.com>
2012-08-20 15:00:04 +0200

    Reorganization

--- documentation/generator.js
+++ documentation/generator.js
@@ -1,12 +1,18 @@
 var generator = require( 'docgenerator' ),
+    path = require( 'path' ),
     fs = require( 'fs' );
 
 // Get all the markdown files in this folder
-var files = fs.readdirSync( '.' );
+var files = fs.readdirSync( path.join( __dirname, 'original' ) );
 
-files = files.filter( function( file ) {
-    return file.substr( -3 ) === '.md';
-});
+files = files
+    .filter( function( file ) {
+        return file.substr( -3 ) === '.md';
+    })
+    .map( function( file ) {
+        return path.join( __dirname, 'original', file );
+    })
+    .sort();
 
 generator
     .set( 'format', 'book' )
